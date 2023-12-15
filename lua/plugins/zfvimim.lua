ac59dde51d947354a05ddc7ee902aaac38208e8b
Jack77793 <jack77793@hotmail.com>
2023-12-15 23:21:49 +0800

    add plugin zfvimim

---
+++ lua/plugins/zfvimim.lua
@@ -0,0 +1,52 @@
+return {
+    {
+        "ZSaberLv0/ZFVimIM",
+        dependencies = {
+            "ZSaberLv0/ZFVimJob",
+            "ZSaberLv0/ZFVimGitUtil",
+            { url = "git@github.com:Jack77793/zfvimim-pinyin-base.git" }
+        },
+        config = function ()
+            vim.cmd([[
+            let g:zf_git_user_email="jack77793@hotmail.com""
+            let g:zf_git_user_name="Jack77793"
+            let g:zf_git_user_token="0"
+            let g:ZFVimIM_cloudAsync_autoCleanup=30
+            let g:ZFVimIM_symbolMap = {
+            \   ' ' : [''],
+            \   '`' : ['·'],
+            \   '!' : ['！'],
+            \   '$' : ['￥'],
+            \   '^' : ['……'],
+            \   '-' : [''],
+            \   '_' : ['——'],
+            \   '(' : ['（'],
+            \   ')' : ['）'],
+            \   '[' : ['【'],
+            \   ']' : ['】'],
+            \   '<' : ['《'],
+            \   '>' : ['》'],
+            \   '\' : ['、'],
+            \   '/' : ['、'],
+            \   ';' : ['；'],
+            \   ':' : ['：'],
+            \   ',' : ['，'],
+            \   '.' : ['。'],
+            \   '?' : ['？'],
+            \   "'" : ['‘', '’'],
+            \   '"' : ['“', '”'],
+            \   '0' : [''],
+            \   '1' : [''],
+            \   '2' : [''],
+            \   '3' : [''],
+            \   '4' : [''],
+            \   '5' : [''],
+            \   '6' : [''],
+            \   '7' : [''],
+            \   '8' : [''],
+            \   '9' : [''],
+            \ }
+            ]])
+        end
+    }
+}
