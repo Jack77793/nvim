3e67bc1a00542dd936217531b9bc2ee6a4f98e13
Jack77793 <jack77793@hotmail.com>
2024-01-06 21:23:08 +0800

    update minimap.lua

--- lua/plugins/minimap.lua
+++ lua/plugins/minimap.lua
@@ -1,13 +1,30 @@
+vim.g.minimap_auto_start = 1
+vim.g.minimap_auto_start_win_enter = 1
+vim.g.minimap_width = 12
+
 return {
     {
         "wfxr/minimap.vim",
         config = function()
+            -- change the highlight color
             vim.cmd([[
-            let g:minimap_auto_start = 1
-            let g:minimap_auto_start_win_enter = 1
-            let g:minimap_width = 10
-            let g:minimap_highlight_search = 1
+            hi mr guibg=#2e3440 guifg=#ffffff guibg=#2e3440 guifg=#ffffff
+            hi mcl guibg=#3b4252 guifg=#ffffff guibg=#3b4252 guifg=#ffffff
             ]])
+            vim.g.minimap_highlight_range = "mr"
+            vim.g.minimap_cursor_color = "mcl"
+            -- prevent the cursor from entering the minimap
+            vim.api.nvim_create_autocmd("WinEnter", {
+                pattern = "*",
+                callback = function()
+                    if vim.fn.bufwinnr("-MINIMAP-") == -1 then
+                        return
+                    end
+                    if vim.fn.winnr() == vim.fn.bufwinnr("-MINIMAP-") then
+                        vim.api.nvim_command("wincmd t")
+                    end
+                end
+            })
         end
     }
 }
