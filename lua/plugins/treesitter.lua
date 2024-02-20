aa797de662292b0d4802719e041a23aa014303a4
Jack77793 <jack77793@hotmail.com>
2024-02-21 05:18:48 +0800

    change load event of treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,6 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        event = "VeryLazy",
         config = function()
             require("nvim-treesitter.configs").setup({
                 ensure_installed = { "bash", "c", "cpp", "lua", "vim", "python", "html", "xml", "css", "yaml", "toml", "ini", "json", "markdown" },
