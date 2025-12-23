a71d3c831d6121ae70f2b9ab98f360c27eab06e3
Jack77793 <jack77793@hotmail.com>
2025-12-23 17:13:59 +0800

    update lsp configurations

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,7 +3,9 @@ return {
         "neovim/nvim-lspconfig",
         event = "VeryLazy",
         config = function()
+            vim.lsp.enable("bashls")
             vim.lsp.enable("clangd")
+            vim.lsp.enable("ltex_plus")
             vim.lsp.enable("lua_ls")
             vim.lsp.enable("pyright")
         end
