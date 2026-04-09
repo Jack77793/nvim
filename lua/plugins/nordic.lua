fb930f96ea1cbbfc31e939ff89c25644f2a365f1
Jack77793 <jack77793@hotmail.com>
2026-04-10 00:04:23 +0800

    update plugin nordic.nvim

--- lua/plugins/nordic.lua
+++ lua/plugins/nordic.lua
@@ -1,10 +1,23 @@
 return {
     {
-        "andersevenrud/nordic.nvim",
+        "AlexvZyl/nordic.nvim",
         lazy = false,
         priority = 1000,
         config = function()
-            vim.cmd.colorscheme "nordic"
+            require("nordic").setup({
+                bold_keywords = true,
+                transparent = {
+                    bg = false,
+                    float = false
+                },
+                bright_border = true,
+                reduced_blue = false,
+                swap_backgrounds = false,
+                telescope = {
+                    style = "flat"
+                }
+            })
+            require("nordic").load()
         end
     }
 }
