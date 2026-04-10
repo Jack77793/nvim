f8485f8d34d0790e7c1f4eeea7901e4fd75bad51
Jack77793 <jack77793@hotmail.com>
2026-04-10 18:16:27 +0800

    add plugin codewindow.nvim

---
+++ lua/plugins/codewindow.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "gorbit99/codewindow.nvim",
+        config = function()
+            require("codewindow").setup({
+                auto_enable = true,
+                max_lines = 6400,
+                minimap_width = 16,
+                screen_bounds = "background",
+                window_border = "none"
+            })
+        end
+    }
+}
