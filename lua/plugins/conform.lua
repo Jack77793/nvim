9c4e6f877267613cd2af8fe91abf177e72221cb1
Jack77793 <jack77793@hotmail.com>
2026-07-19 07:24:34 +0800

    feat: config prettier formatter, add mason plugin

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,12 +3,21 @@ return {
 	config = function()
 		require("conform").setup({
 			formatters_by_ft = {
+				css = { "prettier" },
+				html = { "prettier" },
+				javascript = { "prettier" },
+				json = { "prettier" },
+				jsonc = { "prettier" },
 				lua = { "stylua" },
+				markdown = { "prettier" },
 				python = { "isort", "black" },
 				rust = { "rustfmt" },
+				typescript = { "prettier" },
+				vue = { "prettier" },
+				yaml = { "prettier" },
 			},
 			format_on_save = {
-				timeout_ms = 500,
+				timeout_ms = 1000,
 				lsp_format = "fallback",
 			},
 		})
